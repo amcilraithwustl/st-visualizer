37075d28bfb403d7ec43a936b529509ba2378854
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-04-06 13:54:07 -0500

    Fixed Unit Test

--- UnitTest/ImportTests.cpp
+++ UnitTest/ImportTests.cpp
@@ -21,12 +21,12 @@ namespace ImportTests
 			const auto alignmentValues = importAlignments("C:/Users/Aiden McIlraith/Documents/GitHub/st-visualizer/NMK_F_transformation_pt_coord.csv");
 			const auto results = loadTsv(
 				"C:/Users/Aiden McIlraith/Documents/GitHub/st-visualizer/NMK_20201201_cell_type_coord_allspots.tsv",
-				std::vector<std::string>({ "NMK_F_U1 "," NMK_F_U2 "," NMK_F_U3 "," NMK_F_U4"}),
+				std::vector<std::string>({ "NMK_F_U1","NMK_F_U2","NMK_F_U3","NMK_F_U4" }),
 				1,
 				2,
 				std::pair<unsigned, unsigned>(3, 4),
 				5,
-				std::vector<unsigned>({ 6,7,8,9, 10, 11, 12, 13, 14, 15}),
+				std::vector<unsigned>({ 6,7,8,9, 10, 11, 12, 13, 14, 15 }),
 				60,
 				alignmentValues
 			);
