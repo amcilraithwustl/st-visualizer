b5c84a77351ee1237bdb4d1023e3af40730677f3
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-06-23 23:21:03 -0500

    Adding init logic

--- UnitTest/Contour3DTests.cpp
+++ UnitTest/Contour3DTests.cpp
@@ -11,234 +11,166 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ContourTests
 {
-	TEST_CLASS(ContourTests3D)
-	{
-
-	public:
-
-		TEST_METHOD(Contour3DMathematicaTests)
-		{
-			using json = nlohmann::json;
-			int i = 0;
-
-			auto jsonToMatrix = [](const json& source)
-			{
-				//This should only be used for testing. Not logic safe.
-				std::vector<Eigen::Vector3f> a;
-				a.reserve(source.size());
-				for (int i = 0; i < source.size(); i++)
-				{
-					a.emplace_back(
-						source[i][0],
-						source[i][1],
-						source[i][2]
-					);
-				}
-				return a;
-			};
-
-			auto jsonToVector = [](const json& source)
-			{
-				std::vector<std::vector<float>> ret;
-				for (auto row : source)
-				{
-					std::vector<float> temp;
-					for (auto item : row)
-					{
-						temp.push_back(item);
-					}
-					ret.push_back(temp);
-				}
-				return ret;
-			};
-
-			auto jsonToTets = [](const json& source)
-			{
-				std::vector<std::vector<int>> ret;
-				for (auto row : source)
-				{
-					std::vector<int> temp;
-					for (auto item : row)
-					{
-						temp.push_back(static_cast<int>(item) - 1);//Subtract 1 b/c mathematica indices start a 1
-					}
-					ret.push_back(temp);
-				}
-				return ret;
-			};
-			// read a JSON file
-			std::ifstream file("C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DTest.json");
-			auto a = file.is_open();
-			json j2 = json::parse(file);
-			json ret2 = json::array();
-			for (const auto& j : j2) {
-				std::cout << std::endl << "~~ Begin New Evalutation ~~" << std::endl;
-				auto pts = jsonToMatrix(j[0]);
-				auto vals = jsonToVector(j[1]);
-				
-				Eigen::Matrix3Xf ptsMat(3, pts.size());
-				for(int i = 0; i < pts.size(); i++)
-				{
-					ptsMat.col(i) = std::move(pts[i]);
-				}
-				tetgenio out;
-				tetralizeMatrix(ptsMat, out);
-
-				auto tets = jsonToTets(extractTetMathematicaMesh(out));
-
-				auto [verts, segs, segmats] = contourTetMultiDC(pts, tets, vals);
-				auto ctrs = getContourAllMats3D(verts, segs, segmats, vals[0].size(), 0.04);
-				json ret = json::array();
-				ret.push_back(pts);
-				ret.push_back(vals);
-				ret.push_back(tets);
-				ret.push_back(verts);
-				ret.push_back(segs);
-				ret.push_back(segmats);
-				ret.push_back(ctrs);
-				ret.push_back({});
-				ret2.push_back(ret);
-			}
-			std::ofstream f("C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DResults.json");
-			f << ret2;
-		}
-        TEST_METHOD(Contour3DMathematicaTestsPregenTets)
+    TEST_CLASS(ContourTests3D)
+    {
+    private:
+        std::vector<std::vector<std::vector<int>>> tets_vec;
+        std::vector<std::vector<Eigen::Vector3f>> pts_vec;
+        std::vector<std::vector<std::vector<float>>> vals_vec;
+        std::vector<std::vector<std::vector<int>>> tetsImported_vec;
+    public:
+        TEST_METHOD_INITIALIZE(InitValues)
         {
-			using json = nlohmann::json;
-			int i = 0;
+            using json = nlohmann::json;
 
-			auto jsonToMatrix = [](const json& source)
-			{
-				//This should only be used for testing. Not logic safe.
-				std::vector<Eigen::Vector3f> a;
-				a.reserve(source.size());
-				for (int i = 0; i < source.size(); i++)
-				{
-					a.emplace_back(
-						source[i][0],
-						source[i][1],
-						source[i][2]
-					);
-				}
-				return a;
-			};
+            if (tets_vec.size()) return;
+            tets_vec = {};
+            pts_vec = {};
+            vals_vec = {};
+            tetsImported_vec = {};
 
-			auto jsonToVector = [](const json& source)
-			{
-				std::vector<std::vector<float>> ret;
-				for (auto row : source)
-				{
-					std::vector<float> temp;
-					for (auto item : row)
-					{
-						temp.push_back(item);
-					}
-					ret.push_back(temp);
-				}
-				return ret;
-			};
+            auto jsonToMatrix = [](const json& source)
+            {
+                //This should only be used for testing. Not logic safe.
+                std::vector<Eigen::Vector3f> a;
+                a.reserve(source.size());
+                for(int i = 0; i < source.size(); i++)
+                {
+                    a.emplace_back(
+                        source[i][0],
+                        source[i][1],
+                        source[i][2]
+                    );
+                }
+                return a;
+            };
 
-			auto jsonToTets = [](const json& source)
-			{
-				std::vector<std::vector<int>> ret;
-				for (auto row : source)
-				{
-					std::vector<int> temp;
-					for (auto item : row)
-					{
-						temp.push_back(static_cast<int>(item) - 1);//Subtract 1 b/c mathematica indices start a 1
-					}
-					ret.push_back(temp);
-				}
-				return ret;
-			};
-			// read a JSON file
-			std::ifstream file("C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DTest.json");
-			auto a = file.is_open();
-			json j2 = json::parse(file);
-			json ret2 = json::array();
-			for (const auto& j : j2) {
-				std::cout << std::endl << "~~ Begin New Evalutation ~~" << std::endl;
-				auto pts = jsonToMatrix(j[0]);
-				auto vals = jsonToVector(j[1]);
-				auto tets = jsonToTets(j[2]);
+            auto jsonToVector = [](const json& source)
+            {
+                std::vector<std::vector<float>> ret;
+                for(auto row : source)
+                {
+                    std::vector<float> temp;
+                    for(auto item : row)
+                    {
+                        temp.push_back(item);
+                    }
+                    ret.push_back(temp);
+                }
+                return ret;
+            };
 
-				auto [verts, segs, segmats] = contourTetMultiDC(pts, tets, vals);
-				auto ctrs = getContourAllMats3D(verts, segs, segmats, vals[0].size(), 0.04);
-				json ret = json::array();
-				ret.push_back(pts);
-				ret.push_back(vals);
-				ret.push_back(tets);
-				ret.push_back(verts);
-				ret.push_back(segs);
-				ret.push_back(segmats);
-				ret.push_back(ctrs);
-				ret.push_back({});
-				ret2.push_back(ret);
-			}
-			std::ofstream f("C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DResults.json");
-			f << ret2;
+            auto jsonToTets = [](const json& source)
+            {
+                std::vector<std::vector<int>> ret;
+                for(auto row : source)
+                {
+                    std::vector<int> temp;
+                    for(auto item : row)
+                    {
+                        temp.push_back(static_cast<int>(item) - 1); //Subtract 1 b/c mathematica indices start a 1
+                    }
+                    ret.push_back(temp);
+                }
+                return ret;
+            };
+            // read a JSON file
+            std::ifstream file(
+                "C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DTest.json");
+            json j2 = json::parse(file);
+
+            for(const auto& j : j2)
+            {
+                auto pts = jsonToMatrix(j[0]);
+                pts_vec.push_back(pts);
+                auto vals = jsonToVector(j[1]);
+                vals_vec.push_back(vals);
+                auto tetsImported = jsonToTets(j[2]);
+                tetsImported_vec.push_back(tetsImported);
+                Eigen::Matrix3Xf ptsMat(3, pts.size());
+                for(int i = 0; i < pts.size(); i++)
+                {
+                    ptsMat.col(i) = std::move(pts[i]);
+                }
+                tetgenio out;
+                tetralizeMatrix(ptsMat, out);
+
+                auto tets = jsonToTets(extractTetMathematicaMesh(out)[1]);
+                //Undo mathematica index overadjustment from jsonToTets
+                for(auto& tet:tets)
+                {
+                    for(auto& corner:tet)
+                    {
+                        corner++;
+                    }
+                }
+                tets_vec.push_back(tets);
+            }
         }
 
-		//This test is to set a baseline amount of time it takes to alloc, import, and unalloc data
-		TEST_METHOD(dataImportTimeLag)
-		{
-			using json = nlohmann::json;
-			int i = 0;
+        TEST_METHOD(Contour3DMathematicaTests)
+        {
+            using json = nlohmann::json;
+            int i = 0;
 
-			auto jsonToMatrix = [](const json& source)
-			{
-				//This should only be used for testing. Not logic safe.
-				std::vector<Eigen::Vector3f> a;
-				a.reserve(source.size());
-				for (int i = 0; i < source.size(); i++)
-				{
-					a.emplace_back(
-						source[i][0],
-						source[i][1],
-						source[i][2]
-					);
-				}
-				return a;
-			};
+            json ret2 = json::array();
+            for(int i = 0; i < pts_vec.size(); i++)
+            {
+                auto& pts = pts_vec[i];
+                auto& vals = vals_vec[i];
+                auto& tets = tets_vec[i];
 
-			auto jsonToVector = [](const json& source)
-			{
-				std::vector<std::vector<float>> ret;
-				for (auto row : source)
-				{
-					std::vector<float> temp;
-					for (auto item : row)
-					{
-						temp.push_back(item);
-					}
-					ret.push_back(temp);
-				}
-				return ret;
-			};
+                auto [verts, segs, segmats] = contourTetMultiDC(pts, tets, vals);
+                auto ctrs = getContourAllMats3D(verts, segs, segmats, vals[0].size(), 0.04);
+                json ret = json::array();
+                ret.push_back(pts);
+                ret.push_back(vals);
+                ret.push_back(tets);
+                ret.push_back(verts);
+                ret.push_back(segs);
+                ret.push_back(segmats);
+                ret.push_back(ctrs);
+                ret.push_back({});
+                ret2.push_back(ret);
+            }
+            std::ofstream f(
+                "C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DResults.json");
+            f << ret2;
+        }
 
-			auto jsonToTets = [](const json& source)
-			{
-				std::vector<std::vector<int>> ret;
-				for (auto row : source)
-				{
-					std::vector<int> temp;
-					for (auto item : row)
-					{
-						temp.push_back(static_cast<int>(item) - 1);//Subtract 1 b/c mathematica indices start a 1
-					}
-					ret.push_back(temp);
-				}
-				return ret;
-			};
-			// read a JSON file
-			std::ifstream file("C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DTest.json");
-			auto a = file.is_open();
-			json j2 = json::parse(file);
-		}
+        TEST_METHOD(Contour3DMathematicaTestsPregenTets)
+        {
+            using json = nlohmann::json;
+            int i = 0;
 
-		TEST_METHOD(TetgenMathematicaTests)
-		{
+            json ret2 = json::array();
+            for(int i = 0; i < pts_vec.size(); i++)
+            {
+                auto& pts = pts_vec[i];
+                auto& vals = vals_vec[i];
+                auto& tets = tetsImported_vec[i];
+
+                auto [verts, segs, segmats] = contourTetMultiDC(pts, tets, vals);
+                auto ctrs = getContourAllMats3D(verts, segs, segmats, vals[0].size(), 0.04);
+                json ret = json::array();
+                ret.push_back(pts);
+                ret.push_back(vals);
+                ret.push_back(tets);
+                ret.push_back(verts);
+                ret.push_back(segs);
+                ret.push_back(segmats);
+                ret.push_back(ctrs);
+                ret.push_back({});
+                ret2.push_back(ret);
+            }
+            std::ofstream f(
+                "C:\\Users\\Aiden McIlraith\\Documents\\GitHub\\st-visualizer\\UnitTest\\singleContour3DResults.json");
+            f << ret2;
+        }
+
+
+        TEST_METHOD(TetgenMathematicaTests)
+        {
             srand(0);
             constexpr int numTests = 1;
             constexpr int testRange = 100;
@@ -248,10 +180,10 @@ namespace ContourTests
 
             {
                 auto randPoints = Eigen::Matrix3Xf({
-                    {0,1,0,0},
-                    {0,0,1,0},
-                    {0,0,0,1}
-                    });
+                    {0, 1, 0, 0},
+                    {0, 0, 1, 0},
+                    {0, 0, 0, 1}
+                });
 
 
                 tetgenio out;
@@ -260,7 +192,7 @@ namespace ContourTests
                 finalJson.push_back(extractTetMathematicaMesh(out));
             }
 
-            for (int j = 0; j < numTests; j++)
+            for(int j = 0; j < numTests; j++)
             {
                 auto randPoints = Eigen::Matrix3Xf::Random(3, numPoints + j * 2) * testRange;
                 tetgenio out;
@@ -271,11 +203,10 @@ namespace ContourTests
 
             std::ofstream f(R"(C:\Users\Aiden McIlraith\Documents\GitHub\st-visualizer\tetgenResults.json)");
             f << finalJson;
-		}
+        }
 
-		TEST_METHOD(ContourMathematicaTests)
-		{
-			
-		}
-	};
+        TEST_METHOD(ContourMathematicaTests)
+        {
+        }
+    };
 }
