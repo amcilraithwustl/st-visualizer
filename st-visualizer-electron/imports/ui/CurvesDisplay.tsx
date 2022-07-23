9f2f654d46538bd8fa27fe71b9a104c84d2c9dc6
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-07-23 14:25:49 -0500

    Quick color tweak

--- st-visualizer-electron/imports/ui/CurvesDisplay.tsx
+++ st-visualizer-electron/imports/ui/CurvesDisplay.tsx
@@ -1,5 +1,4 @@
 import {
-  active,
   colors,
   datatype,
   defaultColor,
@@ -7,27 +6,19 @@ import {
   pointToVector,
 } from "../api/constants";
 import * as THREE from "three";
-import { Points } from "./threejsComponents/Points";
 import { Line } from "./threejsComponents/Line";
 import * as React from "react";
-import { Vector3 } from "three";
-import { ThreeElements } from "@react-three/fiber";
 
 export const CurvesDisplay = ({
   center,
   data,
 }: {
   center: THREE.Vector3;
-
   data: datatype;
 }): JSX.Element => {
   const translate = center
     ? ([-center.x, -center.y, -center.z] as const)
     : ([0, 0, 0] as const);
-
-  const testCtrs = data.ctrs2Dvals.flatMap((slice) =>
-    slice.flatMap((ctr) => ctr[0].flatMap((pt) => pointToVector(pt)))
-  );
 
   const ctrs = data.ctrs2Dvals.map((slice) =>
     slice
@@ -42,6 +33,7 @@ export const CurvesDisplay = ({
         val: i,
       }))
   );
+
   const finalContours = ctrs.reduce(
     (acc, slice) => {
       slice.forEach((ctr) => acc[ctr.val].push(...ctr.segments));
@@ -49,16 +41,14 @@ export const CurvesDisplay = ({
     },
     [...new Array(ctrs[1].length)].map(() => [] as THREE.Vector3[])
   );
-  console.log("CONTOURS", ctrs);
   return (
     <>
       {finalContours.flatMap((ctr, i) => {
-        console.log("CONTOUR", ctr);
         return (
           <Line
             key={i}
             points={ctr}
-            color={colors[i]}
+            color={colors[i] || defaultColor}
             translate={translate}
             scale={shrink}
           />
