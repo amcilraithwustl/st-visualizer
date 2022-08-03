35438c1fe8fe88f9ba81c5ad9fcf106dd9e853d5
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-08-02 20:47:39 -0500

    Adding mockup UI

--- st-visualizer-electron/imports/ui/ImportPage/ImportPage.tsx
+++ st-visualizer-electron/imports/ui/ImportPage/ImportPage.tsx
@@ -1,5 +1,79 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
+type importType = {
+  sliceNames: string[]; //Names of each of the slices
+  shrink: number; //Shrink factor
+  alignmentFile: string; //Location of the file
+  fileName: string; //Location of the file
+  slice_index: number; //Index of the slice ID
+  tissueIndex: number; //Index of the tissue type
+  row_col_indices: [number, number]; //Location indices
+  clusterIndex: number; //Index of the cluster value
+  featureCols: number[]; //Columns which old features
+  z_distance: number;
+};
 export const ImportPage = (): JSX.Element => {
-  return <div>Import Page</div>;
+  const [nCols, setNCols] = useState<number>(1);
+  return (
+    <div style={{ padding: 20 }}>
+      <Typography variant="h4">Files</Typography>
+      <Button variant="contained" component="label">
+        Data File
+        <input hidden accept=".tsv" type="file" />
+      </Button>
+      <Button variant="contained" component="label">
+        Alignment File
+        <input hidden accept="text/csv" type="file" />
+      </Button>
+      <Typography variant="h4">Column Management</Typography>
+      <TextField
+        id="outlined-number"
+        label="Number of Columns"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={nCols}
+        onChange={(v) =>
+          parseInt(v.target.value) > 0 && setNCols(parseInt(v.target.value))
+        }
+      />
+      <Grid>
+        {[...new Array(nCols)].map((_, i) => (
+          <FormControl key={i}>
+            <InputLabel id="demo-simple-select-label">
+              {"Column " + (i + 1)}
+            </InputLabel>
+            <Select label={"Column " + (i + 1)} style={{ minWidth: 130 }}>
+              <MenuItem value={1}>Slice</MenuItem>
+              <MenuItem value={2}>Tissue</MenuItem>
+              <MenuItem value={3}>Row Location</MenuItem>
+              <MenuItem value={4}>Column Location</MenuItem>
+              <MenuItem value={6}>Cluster</MenuItem>
+              <MenuItem value={7}>Feature</MenuItem>
+            </Select>
+          </FormControl>
+        ))}
+      </Grid>
+      <Typography variant="h4">Calculation Data</Typography>
+      <TextField
+        id="outlined-number"
+        type="number"
+        label={"Shrink"}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </div>
+  );
 };
