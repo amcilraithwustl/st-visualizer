2eda481b01ee56c07639b27543d998a144119130
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-08-13 18:26:18 -0500

    Adding alignment

--- st-visualizer-electron/imports/ui/AligmentPage/AlignmentPage.tsx
+++ st-visualizer-electron/imports/ui/AligmentPage/AlignmentPage.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Stack, Button } from "@mui/material";
+import { Stack, Button, Grid, IconButton, Tooltip } from "@mui/material";
+import { ArrowLeft, ArrowRight } from "@mui/icons-material";
 export const AlignmentPage = (): JSX.Element => {
-  const [currentImages, setCurrentImages] = useState<File[]>([]);
+  const [currentImages, setCurrentImages] = useState<
+    { file: File; alignments: { x: number; y: number; rotZ: number } }[]
+  >([]);
+  const [selectedImg, setSelectedImg] = useState<number | null>(null);
   const [imgUrls, setImgUrls] = useState<string[]>([]);
   useEffect(() => {
     const func = async () => {
@@ -10,18 +14,74 @@ export const AlignmentPage = (): JSX.Element => {
           (file) =>
             new Promise<string>((res) => {
               const reader = new FileReader();
-              reader.readAsDataURL(file);
+              reader.readAsDataURL(file.file);
               reader.onloadend = function () {
                 res(reader.result as string);
               };
             })
         )
       );
-      console.log("URLS", urls);
       setImgUrls(urls);
     };
     func();
   }, [currentImages]);
+  const imageOrdering = (
+    <Stack direction="row">
+      {imgUrls?.map((img, i) => {
+        return (
+          <Stack
+            direction="row"
+            key={img}
+            style={{
+              borderBottom:
+                selectedImg !== null &&
+                (i === selectedImg || i === selectedImg - 1)
+                  ? "solid 1px black"
+                  : undefined,
+            }}
+          >
+            <Grid item>
+              <IconButton
+                onClick={() => {
+                  if (i === 0) return;
+                  const temp = [...currentImages];
+                  [temp[i], temp[i - 1]] = [temp[i - 1], temp[i]];
+                  setCurrentImages(temp);
+                }}
+              >
+                <ArrowLeft />
+              </IconButton>
+            </Grid>
+            <Tooltip key={img} title={currentImages[i].file.name}>
+              <Grid
+                item
+                sx={{ width: 100 }}
+                onClick={() => i && setSelectedImg(i)}
+              >
+                <img
+                  alt={currentImages[i].file.name}
+                  src={img}
+                  style={{ maxWidth: "100%" }}
+                />
+              </Grid>
+            </Tooltip>
+            <Grid item>
+              <IconButton
+                onClick={() => {
+                  if (i === currentImages.length - 1) return;
+                  const temp = [...currentImages];
+                  [temp[i], temp[i + 1]] = [temp[i + 1], temp[i]];
+                  setCurrentImages(temp);
+                }}
+              >
+                <ArrowRight />
+              </IconButton>
+            </Grid>
+          </Stack>
+        );
+      })}
+    </Stack>
+  );
   return (
     <Stack style={{ padding: 20 }}>
       <Button variant="contained" component="label">
@@ -36,14 +96,14 @@ export const AlignmentPage = (): JSX.Element => {
 
             if (!files) return;
             setCurrentImages(
-              [...new Array(files.length)].map((_, i) => files[i])
+              [...new Array(files.length)]
+                .map((_, i) => files[i])
+                .map((f) => ({ file: f, alignments: { x: 0, y: 0, rotZ: 0 } }))
             );
           }}
         />
       </Button>
-      {imgUrls?.map((img) => {
-        return <img key={img} alt={img} src={img} />;
-      })}
+      {imageOrdering}
     </Stack>
   );
 };
